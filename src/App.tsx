fc2f0c4f2293453fdb9a810243d59b46d981f82c
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-06-28 18:48:37 +0300

    feat: test 5

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
     return (
         <div>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
+            <p>5</p>
+            <p>5</p>
+            <p>5</p>
         </div>
     );
 }
