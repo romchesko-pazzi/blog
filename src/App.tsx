0d06b01f01b10eeff7d5bf005f9cd16865218e4d
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-06-28 11:48:40 +0300

    commit22

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <span>www</span>
+            <span>www</span>
+            <span>www</span>
+            <span>www</span>
+        </div>
+    );
 }
 
 export default App;
