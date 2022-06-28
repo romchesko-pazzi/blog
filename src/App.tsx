4c78c9aa52df30eea39a0cc199c2e99c41c32cfa
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-06-28 15:32:29 +0300

    Revert "Update App.tsx"
    This reverts commit d019571d48dd4ab29a26e3ca4394047525cda2e0.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
     return (
         <div>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
         </div>
     );
 }
