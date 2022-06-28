2606d9be4ce49785bb6216a31ca02e3df6af3a16
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-06-28 13:11:41 +0300

    /

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
     return (
         <div>
-            <span>www</span>
-            <span>www</span>
-            <span>www</span>
-            <span>www</span>
-            <span>www</span>
-            <span>www</span>
+
         </div>
     );
 }
