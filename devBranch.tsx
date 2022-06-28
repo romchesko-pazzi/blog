816e129671fb15f171e90c2a6ba7f0c951ed4c81
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-06-28 12:45:00 +0300

    Update devBranch.tsx

--- devBranch.tsx
+++ devBranch.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const DevBranch = () => {
     return (
         <div>
-            
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
     );
 };
